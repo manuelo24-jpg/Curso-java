1174bf159e2b42ad77efe83dd6f903ac5c1210aa
manuelo69 <manullanorebanal@gmail.com>
2025-10-27 18:15:07 +0100

    añadido imports

--- angular/aprender-angular/src/app/app.ts
+++ angular/aprender-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Usuario } from './usuario/usuario';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Usuario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
