2f76651ff34e3815be4be331c0e0ab054b21b039
manuelo69 <manullanorebanal@gmail.com>
2025-11-04 10:57:17 +0100

    obtencion de usuarios

--- angular/aprender-angular/src/app/services/usuario-service.ts
+++ angular/aprender-angular/src/app/services/usuario-service.ts
@@ -1,3 +1,4 @@
+import id from '@angular/common/locales/extra/id';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,6 +6,20 @@ import { Injectable } from '@angular/core';
 })
 export class UsuarioService {
 
+  usuarios = [
+    { id: 1, nombre: 'Juan Perez', email: 'juan.perez@example.com' },
+    { id: 2, nombre: 'Maria Gomez', email: 'maria.gomez@example.com' },
+    { id: 3, nombre: 'Pedro Martinez', email: 'pedro.martinez@example.com' }
+  ];
 
-  constructor() { }
+  getUsuarios() {
+    {
+      return this.usuarios;
+    }
+  }
+
+  getUsuarioPorId(id: number) {
+    return this.usuarios.find(usuario => usuario.id === id);
+  }
+
 }
